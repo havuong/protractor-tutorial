b6f248b954034e1d27fc4d7ee446ac4a11ca29fe
havuong <havuong138@gmail.com>
2019-05-17 16:38:38 +0700

    Handing Iframes demo

--- conf.js
+++ conf.js
@@ -1,9 +1,9 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['Alert.js'],
+  specs: ['Frames.js'],
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'chrome'
   },
   jasmineNodeOpts: {defaultTimeoutInterval: 50000} // fix - Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
 }
