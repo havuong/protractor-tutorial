a2fbca71f32bb04bb1f837eded93719fbfa85eb6
havuong <havuong138@gmail.com>
2019-05-25 20:58:51 +0700

    [Cucumber] Build common step for multiple scenarios

--- features/demo.feature
+++ features/demo.feature
@@ -2,6 +2,12 @@ Feature: I am going to validate Calculator App
 
 Scenario: Calculator Add functionality testing
 
-Given I will navigate to Calc Site
+Given I will navigate to "Protractor" Site
+# When I add two numbers "3" and "5"
+# Then the output displayed should be "8"
+
+Scenario: Calculator Add functionality testing
+
+Given I will navigate to "Cal" Site
 When I add two numbers "3" and "5"
-Then the output displayed should be "7"
+Then the output displayed should be "8"
--- stepDefinitions/steps.ts
+++ stepDefinitions/steps.ts
@@ -5,8 +5,17 @@ import { calculatorPO } from "../pageObjects/calculatorPO";
 
 let cal = new calculatorPO();
 
-Given('I will navigate to Calc Site', async () => {
-    await browser.get('http://juliemr.github.io/protractor-demo/');
+// Given('I will navigate to Calc Site', async () => {
+//     await browser.get('http://juliemr.github.io/protractor-demo/');
+// });
+
+Given('I will navigate to {string} Site', async (site) => {
+    if (site=='Protractor') {
+        await browser.get('http://www.protractortest.org');
+    } else if (site=='Cal') {
+        await browser.get('http://juliemr.github.io/protractor-demo/');
+    }
+    
 });
 
 When('I add two numbers {string} and {string}', async (string, string2) => {
