5f3e40d989c6b216bb30bfcbe2942bb9ea86fc0a
havuong <havuong138@gmail.com>
2019-05-28 20:06:28 +0700

    [Cucumber] Implement html reporting with onComplete option

--- cucumberConf.ts
+++ cucumberConf.ts
@@ -1,4 +1,5 @@
 import { Config } from "protractor";
+import * as reporter from "cucumber-html-reporter";
 
 export let config: Config = {
   directConnect: true,
@@ -7,20 +8,40 @@ export let config: Config = {
   // path relative to the current config file
   frameworkPath: require.resolve('protractor-cucumber-framework'),
   // require feature files
-  
-    capabilities: {
+
+  capabilities: {
     browserName: 'chrome'
   },
 
   specs: [
-    '../features/demo.feature' // accepts a glob
+    '../features/demo.feature'
   ],
 
   cucumberOpts: {
+    format: 'json:./reports/cucumber_report.json',
     // require step definitions
     require: [
-      './stepDefinitions/*.js' // accepts a glob
+      './stepDefinitions/*.js'
     ],
     // tags: '@caltesting'
+  },
+
+  onComplete: () => {
+    var options = {
+      theme: 'bootstrap',
+      jsonFile: './reports/cucumber_report.json',
+      output: './reports/cucumber_report.html',
+      reportSuiteAsScenarios: true,
+      launchReport: true,
+      metadata: {
+        "App Version": "0.3.2",
+        "Test Environment": "STAGING",
+        "Browser": "Chrome 74.0.3729",
+        "Platform": "Windows 10",
+        "Parallel": "Scenarios",
+        "Executed": "Remote"
+      }
+    };
+    reporter.generate(options);
   }
 }
